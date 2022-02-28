59b97133479a929950adc699310063962b8f11e4
Marco Zampini <marco@bloom-it.io>
2022-02-28 15:55:14 +0100

    Done

--- src/viking.js
+++ src/viking.js
@@ -1,19 +1,95 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0)
+      return `${this.name} has received ${damage} points of damage`
+    else return `${this.name} has died in act of combat`
+  }
+  battleCry() {
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0)
+      return `A Saxon has received ${damage} points of damage`
+    else return `A Saxon has died in combat`
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking)
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon)
+  }
+  vikingAttack() {
+    const chosenSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
+    const chosenVikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
 
+    this.saxonArmy[chosenSaxonIndex].receiveDamage(
+      this.vikingArmy[chosenVikingIndex].strength
+    )
 
+    if (this.saxonArmy[chosenSaxonIndex].health <= 0) {
+      this.saxonArmy.splice(chosenSaxonIndex, 1)
+      return `A Saxon has died in combat`
+    } else
+      return `A Saxon has received ${this.vikingArmy[chosenVikingIndex].strength} points of damage`
+  }
+
+  saxonAttack() {
+    const chosenVikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
+    const chosenSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
+
+    this.vikingArmy[chosenVikingIndex].receiveDamage(
+      this.saxonArmy[chosenSaxonIndex].strength
+    )
+
+    if (this.vikingArmy[chosenVikingIndex].health <= 0) {
+      let deadViking = this.vikingArmy.splice(chosenVikingIndex, 1)
+      return `${deadViking.name} has died in combat`
+    } else
+      return `${this.vikingArmy[chosenVikingIndex].name} has received ${this.saxonArmy[chosenSaxonIndex].strength} points of damage`
+  }
+  showStatus() {
+    if (this.saxonArmy.length === 0)
+      return 'Vikings have won the war of the century!'
+    if (this.vikingArmy.length === 0)
+      return 'Saxons have fought for their lives and survived another day...'
+    return 'Vikings and Saxons are still in the thick of battle.'
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = { Soldier, Viking, Saxon, War };
+  module.exports = { Soldier, Viking, Saxon, War }
 }
